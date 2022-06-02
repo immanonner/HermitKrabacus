440365b02abe1713d8885b8cf711a6e94532a43b
immanonner <76822997+immanonner@users.noreply.github.com>
2022-06-01 21:41:38 -0400

    integrating db and sqlite continued...

--- application/__init__.py
+++ application/__init__.py
@@ -1,12 +1,15 @@
-from flask_assets import Environment
-from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_assets import Environment
 from config import *
 from application.assets import compile_static_assets
 
 
-assets = Environment()
+
 db = SQLAlchemy()
+login_manager = LoginManager()
+assets = Environment()
 
 def init_app():
     """Initialize the core application."""
@@ -17,13 +20,16 @@ def init_app():
 
     # Initialize Plugins
     db.init_app(app)
+    login_manager.init_app(app)
     assets.init_app(app)
     
     with app.app_context():
         
         # Include our Routes to Register all Blueprints
         import application.routes
+        import application.auth
         # bundle (js -> jsmin; less->cssmin)
-        compile_static_assets(assets, default_bp_name="base_bp")
+        if app.config['FLASK_ENV'] == 'development':
+            compile_static_assets(assets, default_bp_name="base_bp")
         db.create_all()
         return app
--- application/models.py
+++ application/models.py
@@ -1,19 +1,17 @@
-
-
+from . import db
+import datetime, time
+from flask_login import UserMixin
 
 # -----------------------------------------------------------------------
 # Database models
 # -----------------------------------------------------------------------
 class Users(db.Model, UserMixin):
     # our ID is the character ID from EVE API
-    character_id = db.Column(
-        db.BigInteger,
+    character_id = db.Column(db.BigInteger,
         primary_key=True,
-        autoincrement=False
-    )
+        autoincrement=False)
     character_owner_hash = db.Column(db.String(255))
     character_name = db.Column(db.String(200))
-
     # SSO Token stuff
     access_token = db.Column(db.String(4096))
     access_token_expires = db.Column(db.DateTime())
@@ -30,17 +28,14 @@ class Users(db.Model, UserMixin):
         return {
             'access_token': self.access_token,
             'refresh_token': self.refresh_token,
-            'expires_in': (
-                    self.access_token_expires - datetime.utcnow()
-            ).total_seconds()
+            'expires_in': (self.access_token_expires - datetime.utcnow()).total_seconds()
         }
 
     def update_token(self, token_response):
         """ helper function to update token data from SSO response """
         self.access_token = token_response['access_token']
         self.access_token_expires = datetime.fromtimestamp(
-            time.time() + token_response['expires_in'],
-        )
+            time.time() + token_response['expires_in'],)
         if 'refresh_token' in token_response:
             self.refresh_token = token_response['refresh_token']
 
