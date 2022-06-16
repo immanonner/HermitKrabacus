54e080051fdb8dd58d1727e15f7b709c9abd620b
ImmaNooner <ssw.williams@gmail.com>
2022-06-16 15:57:10 -0400

    pushing renamed files; updated requirements with autopep8; included bz2 csv file for uncompress implementation

--- application/webassets/market/__init__.py
+++ application/webassets/market/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 from flask_login import current_user, login_required
-import esi_operations
+from . import esi_operations
 
 bp = Blueprint(
     'market_bp', __name__,
